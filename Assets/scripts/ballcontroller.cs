3a4ef5ecc25f12e041be50dc30b06cc291cc9847
rawkush <ankushrawat950@gmail.com>
2018-08-06 19:17:46 +0530

    adding the ball controller script

---
+++ Assets/scripts/ballcontroller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballcontroller : MonoBehaviour {
+    [SerializeField]
+    private float speed;
+
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Up date is called once per frame
+	void Update () {
+		
+	}
+}
