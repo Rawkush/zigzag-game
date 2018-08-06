ca7bb6e85588875249a281144445dec0809825a4
rawkush <ankushrawat950@gmail.com>
2018-08-06 19:27:18 +0530

    switching function
    created a function to switch the direction of the ball moving

--- Assets/scripts/ballcontroller.cs
+++ Assets/scripts/ballcontroller.cs
@@ -8,18 +8,32 @@ public class ballcontroller : MonoBehaviour {
 
     Rigidbody rb;
 
-    private void Awake()
-    {
+    private void Awake() {
         rb = GetComponent<Rigidbody>();
     }
 
     // Use this for initialization
     void Start () {
-		
+        rb.velocity = new Vector3(speed, 0, 0);
 	}
 	
 	// Up date is called once per frame
 	void Update () {
 		
 	}
+
+    void SwitchDirection()
+    {
+        if (rb.velocity.z > 0)
+        {
+            rb.velocity = new Vector3(speed, 0, 0);
+        }
+        else if (rb.velocity.x > 0)
+        {
+            rb.velocity = new Vector3(0, 0, speed);
+        }
+    }
+ 
+    
+
 }
